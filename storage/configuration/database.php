3713916fd3a76d0ef04a3e75ba18b22e545e9b8f
Stephen M. Walker II <stephenwalker@aethelyon.com>
2020-05-12 22:34:49 +0100

    Initial commit

---
+++ storage/configuration/database.php
@@ -0,0 +1,11 @@
+<?php
+
+	$KOKEN_DATABASE = array(
+		'driver' => 'mysqli',
+		'hostname' => 'internal-db.s9159.gridserver.com',
+		'database' => 'db9159_srk',
+		'username' => 'db9159_srk',
+		'password' => 'drew2Ays1Ef1gheck9mud9E9oL4E2oc0aWl2ak1quaps5owD0a',
+		'prefix' => 'koken_',
+		'socket' => ''
+	);
---
+++ storage/configuration/user_setup.php
@@ -0,0 +1,8 @@
+<?php
+	
+	// Path to ImageMagick on your server
+	//define('MAGICK_PATH', 'convert');
+	define('MAGICK_PATH', 'gd');
+	
+	// Path to ffmpeg on your server
+	define('FFMPEG_PATH', 'ffmpeg');
